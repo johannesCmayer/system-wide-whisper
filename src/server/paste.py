95bad5e39298b4b619259cb93cbf7c2100d11225
Johannes C. Mayer <j.c.mayer240@gmail.com>
2023-12-05 19:00:15 +0100

    proper focused window program detection.

--- src/server/paste.py
+++ src/server/paste.py
@@ -10,6 +10,8 @@ import pyperclip
 from data_structures import ThreadState
 from config import config
 
+terminal_names = ['alacritty', 'gnome-terminal', 'xterm', 'konsole', 'kitty', 'terminator', 'guake', 'tilix', 'terminology', 'cool-retro-term', 'tilda', 'terminix', 'terminator', 'xfce4-terminal', 'mate-terminal', 'lxterminal', 'sakura', 'eterm', 'rxvt', 'urxvt', 'st', 'qterminal', 'lilyterm', 'terminator', 'terminator-gtk3', 'terminator-gtk2', 'terminator-gnome', 'terminator-xfce', 'terminator-k']
+
 def _X_get_clipboard():
     result = subprocess.run(["xclip", "-selection", "clipboard", "-out"], 
                             stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
@@ -19,22 +21,28 @@ def _X_get_clipboard():
     else:
         return result.stdout
 
+def _X_get_window_name():
+    return subprocess.check_output([r"""xprop -id $(xdotool getwindowfocus) | sed -n 's/WM_CLASS.*= "\([^"]*\).*/\1/p'"""], shell=True).decode().strip()
+
 def _X_paste_text(text):
     clipboard_contents = _X_get_clipboard()
     #subprocess.run(['xdotool', 'type', text])
-    program = subprocess.check_output(["ps -e | grep $(xdotool getwindowpid $(xdotool getwindowfocus)) | grep -v grep | awk '{print $4}'"], shell=True).decode().strip()
+    program = _X_get_window_name()
     subprocess.run(['xclip', '-selection', 'primary'], input=text.encode(), check=True)
     logging.info(f'program is: {program}')
-    if program.lower() == 'emacs':
-        subprocess.run(['xclip', '-selection', 'clipboard'], input=(text+"").encode(), check=True)
+    if program.lower() in ['emacs']:
+        # Use Shift+Insert
+        subprocess.run(['xclip', '-selection', 'clipboard'], input=(text).encode(), check=True)
         subprocess.check_output(['xdotool', 'key', '--clearmodifiers', 'Shift+Insert'])
-    elif program.lower() == 'discord':
-        subprocess.run(['xclip', '-selection', 'clipboard'], input=(text+" ").encode(), check=True)
-        subprocess.run(['xdotool', 'key', '--clearmodifiers', 'Ctrl+V'], check=True)
+    elif program.lower() in [*terminal_names, 'obsidian', 'code']:
+        # Use Shift+Ctrl+v
+        subprocess.run(['xclip', '-selection', 'clipboard'], input=(text).encode(), check=True)
+        subprocess.run(['xdotool', 'key', '--clearmodifiers', 'Shift+Ctrl+V'], check=True)
         time.sleep(1)
     else:
+        # Use Ctrl+v
         subprocess.run(['xclip', '-selection', 'clipboard'], input=text.encode(), check=True)
-        subprocess.run(['xdotool', 'key', '--clearmodifiers', 'ctrl+V'], check=True)
+        subprocess.run(['xdotool', 'key', '--clearmodifiers', 'Ctrl+v'], check=True)
         time.sleep(0.25)
     subprocess.run(['xclip', '-selection', 'clipboard'], input=clipboard_contents.encode(), check=True)
 
